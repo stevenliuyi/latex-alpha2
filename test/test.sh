8729649584c3792b52cf817ba9c1860be1ec0f2b
stevenliuyi <stevenliuyi@gmail.com>
2019-03-09 19:30:51 -0500

    use the latest .sty file for test

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,7 @@ FILENAME=$1
 docker pull stevenliuyi/latexalpha2
 docker run -dit --name latexalpha2 stevenliuyi/latexalpha2
 docker cp $FILENAME.tex latexalpha2:/latexalpha2
+docker cp ../latexalpha2.sty latexalpha2:/latexalpha2
 docker exec latexalpha2 pdflatex -shell-escape $FILENAME.tex
 docker exec latexalpha2 sh -c "asy $FILENAME-*.asy"
 docker exec latexalpha2 pdflatex -shell-escape $FILENAME.tex
